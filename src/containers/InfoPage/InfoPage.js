77ffb8b73c81e47472094788abb6b827a1ac830c
Sergey Borodin <justtrueserj@gmail.com>
2019-07-18 00:33:02 +0300

    Added template to InfoPage container

---
+++ src/containers/InfoPage/InfoPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class InfoPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is info page</h1>
+      </div>
+    );
+  }
+}
+
+export default InfoPage;
