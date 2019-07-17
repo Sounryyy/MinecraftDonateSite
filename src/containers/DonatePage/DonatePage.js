17d0a46bf0d792200b87fead12806ec8d17c4438
Sergey Borodin <justtrueserj@gmail.com>
2019-07-18 00:32:47 +0300

    Added template to DonatePage container

---
+++ src/containers/DonatePage/DonatePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DonatePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>DonatePage</h1>
+      </div>
+    );
+  }
+}
+
+export default DonatePage;
