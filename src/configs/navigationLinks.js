9cb42f327a8d50500f1684630fec2dc60a81beb8
Sergey Borodin <justtrueserj@gmail.com>
2019-07-18 00:28:05 +0300

    Added configs and navigation links config

---
+++ src/configs/navigationLinks.js
@@ -0,0 +1,44 @@
+const navigationLinks = [
+  {
+    attributes: {
+      href: 'https://vk.com/Sounryyy',
+      id: 'nav-rules',
+      target: 'blank'
+    },
+    text: 'Правила'
+  },
+  {
+    attributes: {
+      href: 'https://vk.com/Sounryyy',
+      id: 'nav-contacts',
+      target: 'blank'
+    },
+    text: 'Контакты'
+  },
+  {
+    attributes: {
+      href: 'https://vk.com/Sounryyy',
+      id: 'nav-security',
+      target: 'blank'
+    },
+    text: 'Безопасность'
+  },
+  {
+    attributes: {
+      href: 'https://vk.com/Sounryyy',
+      id: 'nav-vk-community',
+      target: 'blank'
+    },
+    text: 'Группа VK'
+  },
+  {
+    attributes: {
+      href: 'https://vk.com/Sounryyy',
+      id: 'nav-support',
+      target: 'blank'
+    },
+    text: 'Тех поддержка'
+  }
+];
+
+export default navigationLinks;
