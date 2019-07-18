166426266ced75e406ee83b0235779bbe82dd34c
Sounyyy <jtsvig@gmail.com>
2019-07-18 19:30:16 +0300

    Added styles (failed)

--- src/components/NavigationLink/NavigationLink.js
+++ src/components/NavigationLink/NavigationLink.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'reshadow';
+import styles from './NavigationLink.shadow.css';
 
 class NavigationLink extends Component {
+  static propTypes = {
+    attributes: PropTypes.object.isRequired,
+    text: PropTypes.string.isRequired,
+    color: PropTypes.oneOf(['white', 'black'])
+  };
+
+  static defaultProps = {
+    color: 'white'
+  };
+
   render() {
-    const { attributes, text } = this.props;
-    return (
-      <li>
+    const { attributes, text, color } = this.props;
+    return styled(styles)(
+      <li className="navigation-link">
         <a {...attributes}>{text}</a>
       </li>
     );
   }
 }
-
-NavigationLink.propTypes = {
-  attributes: PropTypes.object.isRequired,
-  text: PropTypes.string.isRequired
-};
 
 export default NavigationLink;
